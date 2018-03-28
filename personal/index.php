ad2f2fb547f305831e2066c2f376ed28c8efaaf9
sv@prime-ltd.su <sputniki@direct.prime-ltd.su>
2018-03-28 16:36:04 +0300

    serv

--- personal/index.php
+++ personal/index.php
@@ -1,8 +1,11 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Персональный раздел");
-?><?$APPLICATION->IncludeComponent("bitrix:sale.personal.section", "sale.personal.section", Array(
-	"ACCOUNT_PAYMENT_ELIMINATED_PAY_SYSTEMS" => array(
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:sale.personal.section", 
+	"sale.personal.section", 
+	array(
+		"ACCOUNT_PAYMENT_ELIMINATED_PAY_SYSTEMS" => array(
 			0 => "0",
 		),
 		"ACCOUNT_PAYMENT_PERSON_TYPE" => "1",
@@ -15,54 +18,61 @@ $APPLICATION->SetTitle("Персональный раздел");
 			4 => "5000",
 			5 => "",
 		),
-		"ACCOUNT_PAYMENT_SELL_USER_INPUT" => "Y",	// Разрешить пользователю вводить сумму
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
-		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
-		"CACHE_TIME" => "3600",	// Время кеширования (сек.)
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CHECK_RIGHTS_PRIVATE" => "N",	// Проверять права доступа
+		"ACCOUNT_PAYMENT_SELL_USER_INPUT" => "Y",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "3600",
+		"CACHE_TYPE" => "A",
+		"CHECK_RIGHTS_PRIVATE" => "N",
 		"COMPATIBLE_LOCATION_MODE_PROFILE" => "N",
-		"CUSTOM_PAGES" => "",	// Настройки дополнительных страниц раздела
-		"CUSTOM_SELECT_PROPS" => "",	// Дополнительные свойства инфоблока
-		"NAV_TEMPLATE" => "",	// Имя шаблона для постраничной навигации
-		"ORDER_HISTORIC_STATUSES" => array(	// Перенести в историю заказы в статусах
+		"CUSTOM_PAGES" => "",
+		"CUSTOM_SELECT_PROPS" => array(
+		),
+		"NAV_TEMPLATE" => "",
+		"ORDER_HISTORIC_STATUSES" => array(
 			0 => "F",
 		),
-		"PATH_TO_BASKET" => "/personal/cart",	// Путь к корзине
-		"PATH_TO_CATALOG" => "/catalog/",	// Путь к каталогу
-		"PATH_TO_CONTACT" => "/kontakty/",	// Путь к странице контактных данных
-		"PATH_TO_PAYMENT" => "/personal/order/payment",	// Путь к странице оплат
+		"PATH_TO_BASKET" => "/personal/cart",
+		"PATH_TO_CATALOG" => "/catalog/",
+		"PATH_TO_CONTACT" => "/kontakty/",
+		"PATH_TO_PAYMENT" => "/personal/order/payment",
 		"PER_PAGE" => "20",
-		"PROP_1" => "",	// Не показывать свойства для типа плательщика "Физическое лицо" (s1)
-		"PROP_2" => "",	// Не показывать свойства для типа плательщика "Юридическое лицо" (s1)
-		"SAVE_IN_SESSION" => "Y",	// Сохранять установки фильтра в сессии пользователя
-		"SEF_FOLDER" => "/personal/",	// Каталог ЧПУ (относительно корня сайта)
-		"SEF_MODE" => "Y",	// Включить поддержку ЧПУ
-		"SEND_INFO_PRIVATE" => "N",	// Генерировать почтовое событие
-		"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
+		"PROP_1" => array(
+		),
+		"PROP_2" => array(
+		),
+		"SAVE_IN_SESSION" => "Y",
+		"SEF_FOLDER" => "/personal/",
+		"SEF_MODE" => "Y",
+		"SEND_INFO_PRIVATE" => "N",
+		"SET_TITLE" => "Y",
 		"SHOW_ACCOUNT_COMPONENT" => "Y",
-		"SHOW_ACCOUNT_PAGE" => "Y",	// Показать страницу персонального счета пользователя
+		"SHOW_ACCOUNT_PAGE" => "N",
 		"SHOW_ACCOUNT_PAY_COMPONENT" => "Y",
-		"SHOW_BASKET_PAGE" => "Y",	// Вывести ссылку на корзину
-		"SHOW_CONTACT_PAGE" => "Y",	// Вывести ссылку на страницу контактов
-		"SHOW_ORDER_PAGE" => "Y",	// Показать страницу заказов пользователя
-		"SHOW_PRIVATE_PAGE" => "Y",	// Показать страницу персональных данных пользователя
-		"SHOW_PROFILE_PAGE" => "N",	// Показать страницу профилей пользователя
-		"SHOW_SUBSCRIBE_PAGE" => "N",	// Показать страницу подписок
+		"SHOW_BASKET_PAGE" => "N",
+		"SHOW_CONTACT_PAGE" => "N",
+		"SHOW_ORDER_PAGE" => "Y",
+		"SHOW_PRIVATE_PAGE" => "Y",
+		"SHOW_PROFILE_PAGE" => "N",
+		"SHOW_SUBSCRIBE_PAGE" => "N",
 		"USER_PROPERTY_PRIVATE" => "",
 		"USE_AJAX_LOCATIONS_PROFILE" => "N",
-		"COMPONENT_TEMPLATE" => ".default",
-		"ORDER_HIDE_USER_INFO" => array(	// Не показывать в информации о пользователе
+		"COMPONENT_TEMPLATE" => "sale.personal.section",
+		"ORDER_HIDE_USER_INFO" => array(
 			0 => "0",
 		),
-		"ORDER_RESTRICT_CHANGE_PAYSYSTEM" => array(	// Запретить смену платежной системы у заказов в статусах
+		"ORDER_RESTRICT_CHANGE_PAYSYSTEM" => array(
 			0 => "0",
 		),
-		"ORDER_DEFAULT_SORT" => "STATUS",	// Сортировка заказов
-		"ORDER_REFRESH_PRICES" => "N",	// Пересчитывать заказ после смены платежной системы
-		"ORDERS_PER_PAGE" => "20",	// Количество заказов на одной странице
+		"ORDER_DEFAULT_SORT" => "STATUS",
+		"ORDER_REFRESH_PRICES" => "N",
+		"ORDERS_PER_PAGE" => "20",
 		"PROFILES_PER_PAGE" => "20",
-		"MAIN_CHAIN_NAME" => "Мой кабинет",	// Название раздела в цепочке навигации
+		"MAIN_CHAIN_NAME" => "Мой кабинет",
+		"ALLOW_INNER" => "N",
+		"ONLY_INNER_FULL" => "N",
+		"COMPOSITE_FRAME_MODE" => "A",
+		"COMPOSITE_FRAME_TYPE" => "AUTO",
 		"SEF_URL_TEMPLATES" => array(
 			"index" => "index.php",
 			"orders" => "orders/",
