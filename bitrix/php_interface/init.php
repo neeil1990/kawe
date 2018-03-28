f73d5535b42c903d6c4e6c860bd86e70586cfb1c
neeil1990 <neeil@mail.ru>
2018-03-28 11:57:19 +0300

    no message

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -6,7 +6,7 @@ function priceDiscount($id){
     global $USER;
     $ar_res_price = CCatalogProduct::GetOptimalPrice($id, 1, $USER->GetUserGroupArray(), 'N');
     if($ar_res_price['DISCOUNT_PRICE']){
-        return array("DISCOUNT" => $ar_res_price['DISCOUNT'], "DISCOUNT_PRICE" => $ar_res_price['DISCOUNT_PRICE']);
+        return array("DISCOUNT" => $ar_res_price['DISCOUNT'], "DISCOUNT_PRICE" => $ar_res_price['DISCOUNT_PRICE']." ₽");
     }else{
         return false;
     }
