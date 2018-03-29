6997c441cc08ea2ccec318eb2e47c97ceb423c64
neeil1990 <neeil@mail.ru>
2018-03-29 11:14:45 +0300

    no message

--- index.php
+++ index.php
@@ -112,68 +112,79 @@ $APPLICATION->SetTitle("АльмаМед - поставки медицинско
                 </div>
 
                 <div class="full-slider">
-                    <ul class="full-slider__wrapper">
-                        <li class="full-slider__item">
-                            <div class="full-slider__item_wrapper">
-                                <div class="full-slider__alert">new</div>
-                                <div class="full-slider__img">
-                                    <img src="img/goods_1.jpg" alt="goods">
-                                </div>
-                                <div class="full-slider__content">
-                                    <div class="full-slider__offer">СПЕЦПРЕДЛОЖЕНИЕ</div>
-                                    <div class="full-slider__rate">
-                                        <i class="icon-star"></i>
-                                        <i class="icon-star"></i>
-                                        <i class="icon-star"></i>
-                                        <i class="icon-star"></i>
-                                        <i class="icon-star-o"></i>
-                                    </div>
-                                    <div class="full-slider__prices">
-                                        <div class="full-slider__price">16 999 Р</div>
-                                        <div class="full-slider__price-old">17 124 Р</div>
-                                        <span>за штуку</span>
-                                    </div>
-                                    <div class="full-slider__categories">Дерматоскоп</div>
-                                    <a href="#" class="full-slider__title">KAWE PICCOLIGHT D 2,5 В - ПИККОЛАЙТ 2,5 В</a>
-                                    <div class="full-slider__article">Арт: 252023а</div>
-                                    <div class="full-slider__info">
-                                        <div class="full-slider__availability">В наличии</div>
-                                        <a href="#" class="full-slider__basket icon-basket"></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="full-slider__item">
-                            <div class="full-slider__item_wrapper">
-                                <div class="full-slider__alert">new</div>
-                                <div class="full-slider__img">
-                                    <img src="img/goods_1.jpg" alt="goods">
-                                </div>
-                                <div class="full-slider__content">
-                                    <div class="full-slider__offer">СПЕЦПРЕДЛОЖЕНИЕ</div>
-                                    <div class="full-slider__rate">
-                                        <i class="icon-star"></i>
-                                        <i class="icon-star"></i>
-                                        <i class="icon-star"></i>
-                                        <i class="icon-star"></i>
-                                        <i class="icon-star-o"></i>
-                                    </div>
-                                    <div class="full-slider__prices">
-                                        <div class="full-slider__price">16 999 Р</div>
-                                        <div class="full-slider__price-old">17 124 Р</div>
-                                        <span>за штуку</span>
-                                    </div>
-                                    <div class="full-slider__categories">Дерматоскоп</div>
-                                    <a href="#" class="full-slider__title">KAWE PICCOLIGHT D 2,5 В - ПИККОЛАЙТ 2,5 В</a>
-                                    <div class="full-slider__article">Арт: 252023а</div>
-                                    <div class="full-slider__info">
-                                        <div class="full-slider__availability">В наличии</div>
-                                        <a href="#" class="full-slider__basket icon-basket"></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+
+                    <?$APPLICATION->IncludeComponent(
+                    "bitrix:news.list",
+                    "slider.product",
+                    array(
+                        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                        "ADD_SECTIONS_CHAIN" => "N",
+                        "AJAX_MODE" => "N",
+                        "AJAX_OPTION_ADDITIONAL" => "",
+                        "AJAX_OPTION_HISTORY" => "N",
+                        "AJAX_OPTION_JUMP" => "N",
+                        "AJAX_OPTION_STYLE" => "Y",
+                        "CACHE_FILTER" => "N",
+                        "CACHE_GROUPS" => "Y",
+                        "CACHE_TIME" => "36000000",
+                        "CACHE_TYPE" => "A",
+                        "CHECK_DATES" => "Y",
+                        "DETAIL_URL" => "",
+                        "DISPLAY_BOTTOM_PAGER" => "Y",
+                        "DISPLAY_DATE" => "Y",
+                        "DISPLAY_NAME" => "Y",
+                        "DISPLAY_PICTURE" => "Y",
+                        "DISPLAY_PREVIEW_TEXT" => "Y",
+                        "DISPLAY_TOP_PAGER" => "N",
+                        "FIELD_CODE" => array(
+                            0 => "",
+                            1 => "",
+                        ),
+                        "FILTER_NAME" => "",
+                        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                        "IBLOCK_ID" => "35",
+                        "IBLOCK_TYPE" => "sliders",
+                        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                        "INCLUDE_SUBSECTIONS" => "Y",
+                        "MESSAGE_404" => "",
+                        "NEWS_COUNT" => "20",
+                        "PAGER_BASE_LINK_ENABLE" => "N",
+                        "PAGER_DESC_NUMBERING" => "N",
+                        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                        "PAGER_SHOW_ALL" => "N",
+                        "PAGER_SHOW_ALWAYS" => "N",
+                        "PAGER_TEMPLATE" => ".default",
+                        "PAGER_TITLE" => "Новости",
+                        "PARENT_SECTION" => "",
+                        "PARENT_SECTION_CODE" => "",
+                        "PREVIEW_TRUNCATE_LEN" => "",
+                        "PROPERTY_CODE" => array(
+                            0 => "ARTICL",
+                            1 => "GROUP_PRODUCT",
+                            2 => "COUNT",
+                            3 => "NAME_PRODUCT",
+                            4 => "SALE",
+                            5 => "NEW_PRICE",
+                            6 => "OLD_PRICE",
+                            7 => "",
+                        ),
+                        "SET_BROWSER_TITLE" => "N",
+                        "SET_LAST_MODIFIED" => "N",
+                        "SET_META_DESCRIPTION" => "N",
+                        "SET_META_KEYWORDS" => "N",
+                        "SET_STATUS_404" => "N",
+                        "SET_TITLE" => "N",
+                        "SHOW_404" => "N",
+                        "SORT_BY1" => "ACTIVE_FROM",
+                        "SORT_BY2" => "SORT",
+                        "SORT_ORDER1" => "DESC",
+                        "SORT_ORDER2" => "ASC",
+                        "STRICT_SECTION_CHECK" => "N",
+                        "COMPONENT_TEMPLATE" => "slider.product"
+                    ),
+                    false
+                );?>
+
                 </div>
 
                 <div class="categories">
