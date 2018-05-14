b6478024f8a7cd56068ee62fd9b34d6e668e916b
sv@prime-ltd.su <sputniki@direct.prime-ltd.su>
2018-05-14 11:09:34 +0300

    srv

--- .section.php
+++ .section.php
@@ -1,8 +1,6 @@
 <?
-$sSectionName = "Главная страница";
-$arDirProperties = array(
-   "description" => "Мы предлагаем широкий ассортимент качественной одежды по адекватным ценам.",
-   "keywords" => "женская одежда, мужская одежда, спортивная одежда, тапочки",
-   "robots" => "index, follow"
+$sSectionName = "Главная ";
+$arDirProperties = Array(
+   "ROBOTS" => "index, follow"
 );
 ?>
