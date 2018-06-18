390ea1dd6f3ccc821b582247918974677bb20578
neeil1990 <neeil@mail.ru>
2018-06-18 11:03:01 +0300

    no message

--- bitrix/templates/medical-templates/components/bitrix/news.list/slider.product/template.php
+++ bitrix/templates/medical-templates/components/bitrix/news.list/slider.product/template.php
@@ -45,7 +45,7 @@ $this->setFrameMode(true);
 				<a href="#" class="full-slider__title"><?=$arItem["PROPERTIES"]["NAME_PRODUCT"]["VALUE"]?></a>
 				<div class="full-slider__article"><?=$arItem["PROPERTIES"]["ARTICL"]["VALUE"]?></div>
 				<div class="full-slider__info">
-					<a href="#" class="subscribe__btn" style="text-decoration: none">Подробнее</a>
+					<a href="<?=$arItem['CODE']?>" class="subscribe__btn" style="text-decoration: none">Подробнее</a>
 				</div>
 			</div>
 		</div>
