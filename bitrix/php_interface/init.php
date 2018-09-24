fb4b99c13ce98dfd3d41073e77ca9602c747b861
neeil1990 <neeil@mail.ru>
2018-09-24 17:27:57 +0300

    no message

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -83,3 +83,24 @@ function bxModifySaleMails($orderID, &$eventName, &$arFields)
   }
   $arFields["PHONE"] =  $phone;
 }
+
+
+// регистрируем обработчик
+AddEventHandler("search", "BeforeIndex", "BeforeIndexHandler");
+// создаем обработчик события "BeforeIndex"
+function BeforeIndexHandler($arFields)
+{
+    if(!CModule::IncludeModule("iblock")) // подключаем модуль
+        return $arFields;
+    if($arFields["MODULE_ID"] == "iblock")
+    {
+        $db_props = CIBlockElement::GetProperty(                        // Запросим свойства индексируемого элемента
+            $arFields["PARAM2"],         // BLOCK_ID индексируемого свойства
+            $arFields["ITEM_ID"],          // ID индексируемого свойства
+            array("sort" => "asc"),       // Сортировка (можно упустить)
+            Array("CODE" => "CML2_ARTICLE")); // CODE свойства (в данном случае артикул)
+        if($ar_props = $db_props->Fetch())
+            $arFields["TITLE"] .= " ".$ar_props["VALUE"];   // Добавим свойство в конец заголовка индексируемого элемента
+    }
+    return $arFields; // вернём изменения
+}
