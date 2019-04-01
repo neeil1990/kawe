4ff726e674fafb779379d32ca23dd4423c6cab49
sv@prime-ltd.su <sputniki@direct.prime-ltd.su>
2019-04-01 11:40:36 +0300

    srv

--- oplata/content.php
+++ oplata/content.php
@@ -2,22 +2,31 @@
  <b>Мы работаем по 100% предоплате, с физическими и юридическими лицами.&nbsp;</b><br>
  <br>
 	 Оплата производится по выставленному счету безналично. Заказ передается в доставку после фактического поступления средств на наш расчетный счет.<br>
-	 Перед началом сотрудничества заключается договор о поставке с прописанными гарантиями покупателя, сроками доставки и взаимодействии сторон.&nbsp;<br>
  <br>
 	 Уже более 7 лет мы поставляем продукцию KaWe покупателям и процессы отлажены до мелочей - от формирования КП до передачи товара клиенту.&nbsp;<br>
-	<br>
-	Если у вам кажется, что вам долго или не достаточно емко отвечает специалист - директор ждет вашу претензию на свою&nbsp;личную почту <a href="mailto:director@kawe.su">director@kawe.su</a>&nbsp;для рассмотрения и решения ситуации.<br>
-	<br>
-	Мы рады долгосрочному сотрудничеству!<br>
+ <br>
+	 Если у вам кажется, что вам долго или не достаточно емко отвечает специалист - директор ждет вашу претензию на свою&nbsp;личную почту <a href="mailto:director@kawe.su">director@kawe.su</a>&nbsp;для рассмотрения и решения ситуации.<br>
+ <br>
+ <b>Оплата и условия поставки без дополнительных договоров</b>
+</p>
+<p>
+	 В случае оплаты счета без заключения дополнительнительного договора на индивидуальных условиях, вы соглашаетесь с условиями договора оферты:&nbsp;<noindex><a href="https://kawe.su/upload/oferta-kawe.pdf" rel="nofollow" target="_blank">https://kawe.su/upload/oferta-kawe.pdf</a></noindex><br>
+ <br>
+	 Мы рады долгосрочному сотрудничеству!<br>
  <br>
 	 &nbsp;
 </p>
-<br>
-<div style="float: left; padding: 5px; margin-right: 10px; margin-left: 0px;" align="justify"><img src="/upload/medialibrary/80a/80aa96b067db42e7b27bef97e23e5f68.jpg" align="middle" style="width: 350px; height: 350px;">
+ <br>
+<div style="float: left; padding: 5px; margin-right: 10px; margin-left: 0px;" align="justify">
+ <img src="/upload/medialibrary/80a/80aa96b067db42e7b27bef97e23e5f68.jpg" align="middle" style="width: 350px; height: 350px;">
 </div>
-<div style="float: left; padding: 5px; margin-right: 10px;"><img src="/upload/medialibrary/333/33347ab67c1c26c2ed8fd976a655f60a.jpg" style="width: 350px; height: 350px;">
+<div style="float: left; padding: 5px; margin-right: 10px;">
+ <img src="/upload/medialibrary/333/33347ab67c1c26c2ed8fd976a655f60a.jpg" style="width: 350px; height: 350px;">
 </div>
-<div style="float: left; padding: 5px; margin-right: 10px;"><img src="/upload/medialibrary/603/60346ef1667ab175dd4194cbf8ac0cd2.jpg" style="width: 350px; height:350px;"> 
+<div style="float: left; padding: 5px; margin-right: 10px;">
+ <img src="/upload/medialibrary/603/60346ef1667ab175dd4194cbf8ac0cd2.jpg" style="width: 350px; height:350px;">
 </div>
-<div style="float: left; padding: 5px; margin-right: 10px;"><img src="/upload/medialibrary/603/kawe-prod.jpg" style="width: 350px; height:350px;"> 
+<div style="float: left; padding: 5px; margin-right: 10px;">
+ <img src="/upload/medialibrary/603/kawe-prod.jpg" style="width: 350px; height:350px;">
 </div>
+ <br>
