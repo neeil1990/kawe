2eaae5e0148e72a9be7b51b4e2f383104fb465b6
neeil1990 <neeil@mail.ru>
2019-05-28 10:47:19 +0300

    cupon

--- bitrix/templates/.default/components/bitrix/sale.discount.coupon.mail/.discount.coupon.mail/template.php
+++ bitrix/templates/.default/components/bitrix/sale.discount.coupon.mail/.discount.coupon.mail/template.php
@@ -4,10 +4,4 @@
 /** @var array $arResult */
 /** @global CDatabase $DB */
 ?>
-<table width="100%" cellpadding="0" cellspacing="0">
-    <tbody>
-        <tr><td colspan="5" height="25"></td></tr>
-        <tr><td align="center"><? echo $arResult['COUPON']; ?></td></tr>
-        <tr><td colspan="5" height="25"></td></tr>
-    </tbody>
-</table>
+<p style="text-align: center;margin: 20px;"><? echo $arResult['COUPON']; ?></p>
