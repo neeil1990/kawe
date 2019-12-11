0e84a3d77c2b7bd2155ff67d975c8377711e87cd
sv@prime-ltd.su <sputniki@direct.prime-ltd.su>
2019-12-11 17:04:34 +0300

    srv

--- bitrix/modules/prime.roistatbitrixcms/lib/classes.php
+++ bitrix/modules/prime.roistatbitrixcms/lib/classes.php
@@ -9,35 +9,34 @@ class RoiStat {
 
         $module = "prime.roistatbitrixcms";
 
-        if(empty(COption::GetOptionString($module, "RoiProxyLeads")) ||
-            empty(COption::GetOptionString($module, "RoiEvent")))
-            return false;
+        if(!empty(COption::GetOptionString($module, "RoiProxyLeads")) ||
+            !empty(COption::GetOptionString($module, "RoiEvent"))){
 
-        $RoiEvent = unserialize(COption::GetOptionString($module, "RoiEvent"));
+            $RoiEvent = unserialize(COption::GetOptionString($module, "RoiEvent"));
 
-        $mess = "";
-        foreach($arFields as $keyField => $arField)
-            $mess .= $keyField .':'. $arField.'; ';
+            $mess = "";
+            foreach($arFields as $keyField => $arField)
+                $mess .= $keyField .':'. $arField.'; ';
 
-        $roistatData = array(
-            'roistat' => isset($_COOKIE['roistat_visit']) ? $_COOKIE['roistat_visit'] : 'nocookie',
-            'key'     => COption::GetOptionString('prime.roistatbitrixcms', "RoiProxyLeads"),
-            'title' => $arTemplate['ID'].' : '.$arTemplate['EVENT_NAME'],
-            'name' =>  $arFields['FIO'] . $arFields['NAME'] . $arFields['ORDER_USER'],
-            'comment' => strip_tags($mess),
-            'phone'   => $arFields['PHONE'],
-            'email'   => $arFields['EMAIL'],
-            'fields'  => array(
-                "price" => $arFields['PRICE'],
-                "bcc" => $arTemplate['BCC'],
-                "request" => $_SERVER['REQUEST_URI'],
-            ),
-        );
+            $roistatData = array(
+                'roistat' => isset($_COOKIE['roistat_visit']) ? $_COOKIE['roistat_visit'] : 'nocookie',
+                'key'     => COption::GetOptionString('prime.roistatbitrixcms', "RoiProxyLeads"),
+                'title' => $arTemplate['ID'].' : '.$arTemplate['EVENT_NAME'],
+                'name' =>  $arFields['FIO'] . $arFields['NAME'] . $arFields['ORDER_USER'],
+                'comment' => strip_tags($mess),
+                'phone'   => $arFields['PHONE'],
+                'email'   => $arFields['EMAIL'],
+                'fields'  => array(
+                    "price" => $arFields['PRICE'],
+                    "bcc" => $arTemplate['BCC'],
+                    "request" => $_SERVER['REQUEST_URI'],
+                ),
+            );
 
-        try {
-            if(in_array($arTemplate['ID'], $RoiEvent))
-                file_get_contents("https://cloud.roistat.com/api/proxy/1.0/leads/add?" . http_build_query($roistatData));
-        } catch (Exception $e) {}
+            try {
+                if(in_array($arTemplate['ID'], $RoiEvent))
+                    file_get_contents("https://cloud.roistat.com/api/proxy/1.0/leads/add?" . http_build_query($roistatData));
+            } catch (Exception $e) {}
+        }
     }
-
 }
