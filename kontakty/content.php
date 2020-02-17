80b6d2b2e6b917e4665bc6bd810838b467c04cb3
sv@prime-ltd.su <sputniki@direct.prime-ltd.su>
2020-02-17 16:19:48 +0300

    srv

--- kontakty/content.php
+++ kontakty/content.php
@@ -1,17 +1,23 @@
 <p>
-	Мы осуществляем доставку по всей России, подробнее об этом в <a href="/dostavka/">Доставка</a>.&nbsp;<br>
-	<br>
-	E-mail для заказов и справки: <a href="mailto:info@kawe.su" class="roi_visit">info@kawe.su</a>
+	 Мы осуществляем доставку по всей России, подробнее об этом в <a href="/dostavka/">Доставка</a>.&nbsp;<br>
+ <br>
+	 E-mail для заказов и справки: <a href="mailto:info@kawe.su" class="roi_visit">info@kawe.su</a>
 </p>
 <p>
-	Письмо директору: <a href="mailto:director@kawe.su">director@kawe.su</a>
+	 Письмо директору: <a href="mailto:director@kawe.su">director@kawe.su</a>
 </p>
 <p>
-	Телефон: <img src="/upload/phone.jpg" style="margin: 0px 0px 0px 0px;">
+	 Телефон: <img src="/upload/phone.jpg" style="margin: 0px 0px 0px 0px;">
 </p>
 <p>
-	График работы менеджеров:  пн-пт 9:00-20:00
+	 График работы менеджеров: пн-пт 9:00-20:00
 </p>
-
-<p> <img src="/upload/kawe-su.jpg" style="width:100%;">
+<p>
+	 Самовывоз осуществляется по согласованию с менеджером только из Воронежского филиала по адресу:
+</p>
+<div style="display: flex; justify-content: space-around; flex-wrap: wrap;">
+ <img width="400" src="/upload/medialibrary/c0c/c0c3c4e8d449d469299b50efcbd91b6f.png">
+</div>
+<p>
+ <img src="/upload/kawe-su.jpg" style="width:100%;">
 </p>
--- urlrewrite.php
+++ urlrewrite.php
@@ -40,20 +40,20 @@ $arUrlRewrite=array (
     'PATH' => '/personal/order/index.php',
     'SORT' => 100,
   ),
-  2 => 
-  array (
-    'CONDITION' => '#^/personal/#',
-    'RULE' => '',
-    'ID' => 'bitrix:sale.personal.section',
-    'PATH' => '/personal/index.php',
-    'SORT' => 100,
-  ),
   17 => 
   array (
     'CONDITION' => '#^/articles/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/articles/index.php',
+    'SORT' => 100,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/personal/#',
+    'RULE' => '',
+    'ID' => 'bitrix:sale.personal.section',
+    'PATH' => '/personal/index.php',
     'SORT' => 100,
   ),
   14 => 
